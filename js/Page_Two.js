4a2b9e87dea562bd1aff1a28e98e009456d140b2
Jeremy Ward <jeremy2929@twc.com>
2017-04-22 04:52:12 -0500

    minor adjustments to agent info

--- js/Page_Two.js
+++ js/Page_Two.js
@@ -53,7 +53,7 @@ export default React.createClass({
         <section className="page2_left_area">
           <h1 className="page2_name_title">Johnny Ramirez</h1>
           <p className="page2_text">I am very proud of my service in the US Marine Corps and US Army. As a successful Real Estate Entrepreneur-my #1 objective is to help families achieve their real estate goals in selling or buying their home. </p>
-          <p className="page2_text">With that said, not all real estate agents are created equal. .</p>
+          <p className="page2_text">With that said, not all real estate agents are created equal.</p>
           <p className="page2_text">I have aligned myself with some of the most successful experts in the real estate industry in San Antonio to help me to provide the best resources for my clients. I am a no nonsense business professional who will always put the needs of my clients first and foremost.</p>
           <img className="page2_image_box" src="/styles/house1.jpg"/>
           <img className="page2_image_box" src="/styles/house2.jpg"/>
