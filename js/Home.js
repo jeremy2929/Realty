bf59f66846fced32fd5debd9165899ce632c128f
Jeremy Ward <jeremy2929@twc.com>
2017-05-11 10:15:08 -0500

    All portrait images use same CSS class

--- js/Home.js
+++ js/Home.js
@@ -62,7 +62,7 @@ export default React.createClass({
                 Buying a home means finding the picture perfect dream home.  This requires an elite real estate agent with an extensive network who understands the importance of trust.
                 </p>
               </div>
-              <img className="services_johnny_portait" src="/styles/Johnny_Portrait.jpg"/>
+              <img className="johnny_portrait" src="/styles/Johnny_Portrait.jpg"/>
             </section>
             <section className="hidden" ref="aboutMe">
                 <img className="close_x" src="/styles/close.png" onClick={this.mainAboutMeDisplay}/>
@@ -71,7 +71,7 @@ export default React.createClass({
                 <p className="aboutMe_text">I am very proud of my service in the US Marine Corps and US Army. As a successful Real Estate Entrepreneur-my #1 objective is to help families achieve their real estate goals in selling or buying their home. </p>
                 <p className="aboutMe_text">With that said, not all real estate agents are created equal.</p>
                 <p className="aboutMe_text">I have aligned myself with some of the most successful experts in the real estate industry in San Antonio to help me to provide the best resources for my clients. I am a no nonsense business professional who will always put the needs of my clients first and foremost.</p>
-              <img className="aboutMe_johnny_portait" src="/styles/Johnny_Portrait.jpg"/>
+              <img className="johnny_portrait" src="/styles/Johnny_Portrait.jpg"/>
             </section>
             <section className="hidden" ref="contactMe">
               <img className="close_x" src="/styles/close.png" onClick={this.mainContactMeDisplay}/>
@@ -103,7 +103,7 @@ export default React.createClass({
                   </tr>
                 </tbody>
               </table>
-              <img className="contactMe_johnny_portait" src="/styles/Johnny_Portrait.jpg"/>
+              <img className="johnny_portrait" src="/styles/Johnny_Portrait.jpg"/>
             </section>
           </div>
         </div>
